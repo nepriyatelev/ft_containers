f077a439a110882fbfdd8e9997bdd114680e27f7
Mitsue Odysseus <modysseu@ga-a2.msk.21-school.ru>
2022-10-11 20:26:08 +0300

    Create stack container

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stack>
+#include "Stack.hpp"
+#include <iostream>
+int main() {
+    std::stack<int> a;
+    ft::stack<int> b;
+//     for (size_t i = 0; i < 10; i++)
+//     {
+//         b.push(i);
+//     }
+//     std::cout << b.size() << std::endl;
+}
