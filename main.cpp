f5272c8891dcd2082c48e5c2416fdaaab9175c4d
Mitsue Odysseus <modysseu@ga-a2.msk.21-school.ru>
2022-10-12 13:12:42 +0300

    stack is done

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 #include "Stack.hpp"
 #include <iostream>
 int main() {
-    std::stack<int> a;
+    ft::stack<int> a;
     ft::stack<int> b;
-//     for (size_t i = 0; i < 10; i++)
-//     {
-//         b.push(i);
-//     }
-//     std::cout << b.size() << std::endl;
+    for (size_t i = 0; i < 10; i++)
+    {
+        b.push(i);
+        a.push(i);
+    }
+    std::cout << b.size() << std::endl;
+    std::cout << b.empty() << std::endl;
+    std::cout << b.top() << std::endl;
+    if (a == b)
+    std::cout << q << std::endl;
 }
